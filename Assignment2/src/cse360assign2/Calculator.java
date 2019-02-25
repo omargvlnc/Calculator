d80c6a6dfe502053318c254ad8138360b0f9408d
Omar <Omar@KHALEESI-PU>
2019-02-25 11:22:35 -0700

    Commit version 3

--- Assignment2/src/cse360assign2/Calculator.java
+++ Assignment2/src/cse360assign2/Calculator.java
@@ -9,12 +9,14 @@ package cse360assign2;
 public class Calculator {
 	 
 	private int total;
+	private String history;
 	 
 	/**
 	 * Calculator constructor
 	 */
 	public Calculator () {
 		total = 0;  
+		history = "0 ";
 	}
 	 
 	/**
@@ -27,39 +29,50 @@ public class Calculator {
 	}
 	
 	/**
-	 * Adds the given value to the total value
+	 * Adds the given value to the total value and updates history string
 	 * 
 	 * @param value	the value to be added to the total
 	 */
 	public void add (int value) {
 		this.total += value;
+		String addStr = Integer.toString(value);
+		this.history += addStr;
 	}
 	
 	/**
-	 * Subtracts the given value from the total value
+	 * Subtracts the given value from the total value and updates history string
 	 * 
 	 * @param value the value to be subtracted from the total
 	 */
 	public void subtract (int value) {
 		this.total -=  value;
+		String subStr = Integer.toString(value);
+		this.history += subStr;
 	}
 	
 	/**
-	 * Multiplies the total by the given value
+	 * Multiplies the total by the given value and updates history string
 	 * 
 	 * @param value the value to divide the total by
 	 */
 	public void multiply (int value) {
 		this.total *= value;
+		String mulStr = Integer.toString(value);
+		this.history += mulStr;
 	}
 	
 	/**
-	 * Divides the total by the given value
+	 * Divides the total by the given value and updates the history string
 	 * 
 	 * @param value the value to multiply the total by
 	 */
 	public void divide (int value) {
-		this.total /= value;
+		if (value == 0)
+			this.total = 0;
+		else
+			this.total /= value; 
+		String divStr = Integer.toString(value);
+		this.history += divStr;
 	}
 	
 	/**
@@ -68,6 +81,6 @@ public class Calculator {
 	 * @return a string representing history of operations performed
 	 */
 	public String getHistory () {
-		return "";
+		return this.history;
 	}
 }
