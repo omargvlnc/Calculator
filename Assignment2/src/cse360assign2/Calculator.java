f3de197848b020836fe5443cf982eb0cb44749e0
Omar <Omar@KHALEESI-PU>
2019-02-25 07:08:51 -0700

    Commit number 1

---
+++ Assignment2/src/cse360assign2/Calculator.java
@@ -0,0 +1,73 @@
+/* Name: Omar Valencia
+ * Class ID: 573
+ * Assignment 2: First version of Calculator class which has methods to perform 
+ * basic operations, such as: addition, subtraction, multiplication, and division.
+ */
+
+package cse360assign2;
+
+public class Calculator {
+	 
+	private int total;
+	 
+	/**
+	 * Calculator constructor
+	 */
+	public Calculator () {
+		total = 0;  
+	}
+	
+	/**
+	 * Gets the current total 
+	 * 
+	 * @return the current total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Adds the given value to the total value
+	 * 
+	 * @param value	the value to be added to the total
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Subtracts the given value from the total value
+	 * 
+	 * @param value the value to be subtracted from the total
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * Multiplies the total by the given value
+	 * 
+	 * @param value the value to divide the total by
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * Divides the total by the given value
+	 * 
+	 * @param value the value to multiply the total by
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * Returns a symbolic string of operations that have been performed
+	 * 
+	 * @return a string representing history of operations performed
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
